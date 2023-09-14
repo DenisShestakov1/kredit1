9f44a975e3e43162dcf1ad4ad65772f11e207aa3
r9jgfja <112956409+DenisShestakov1@users.noreply.github.com>
2023-09-14 17:21:38 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    double loan_amount;
-    int loan_term;
-    double interest_rate;
+    double loan_amount = 0;
+    int loan_term = 0;
+    double interest_rate = 0;
     string loan_type;
+ 
 
     cout << "Введите общую сумму кредита: ";
     cin >> loan_amount;
@@ -19,29 +20,34 @@ int main() {
     cout << "Введите процентную ставку (годовую): ";
     cin >> interest_rate;
 
-    cout << "Введите тип кредита (1-аннуитетный или 2-дифференцированный): ";
+    cout << "Введите тип кредита (1-дифференцированный или 2-аннуитетный): ";
     cin >> loan_type;
 
-    double monthly_payment, total_payment, overpayment, principal ,interest_payment, principal_payment, term ,total_payment2  ;
-
+    double monthly_payment = 0, total_payment = 0, overpayment = 0;
+    //double payments[] = new double[loan_term];
+    double* payments;
+    payments = (double*)malloc(loan_term * sizeof(double));
     if (loan_type == "1") {
+        double ostatok = loan_amount;
+        long double monthly_rate = interest_rate / (12 * 100);
+        for (int i = 1; i <= loan_term; i++) {
+            
+            double residue = loan_amount - (loan_amount / loan_term) * (i - 1);
+            payments[i - 1] = loan_amount / loan_term + residue * monthly_rate;
+        }
+        
+        for (int i = 0; i < loan_term; i++) {
+            cout << payments[i] << endl;
+        }
+
+    }
+    else if (loan_type == "2") {
+     
+
         double monthly_interest_rate = interest_rate / 12 / 100;
         monthly_payment = loan_amount * (monthly_interest_rate / (1 - pow(1 + monthly_interest_rate, -loan_term)));
         total_payment = monthly_payment * loan_term;
         overpayment = total_payment - loan_amount;
-    }
-    else if (loan_type == "2") {
-        double monthly_interest_rate = interest_rate / 12 / 100;
-
-    double interest_payment = principal * monthly_interest_rate;
-
-   
-    double principal_payment = principal / term;
-
-
-    double total_payment2 = principal_payment + interest_payment;
-
-
     }
     else {
         cout << "Неверный тип кредита." << endl;
